cd9e02b4a80c580d68bd80c79b8c02d4e545e2a8
ZOHAIB7689 <bzohaib754@gmail.com>
2024-12-26 18:34:51 +0500

    main

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,24 @@ import React from "react";
 const Navbar = () => {
   return (
     <header className="flex justify-between px-10 py-5 md:px-36  mx-auto">
-      <Link href={"/"} className="text-3xl font-bold hover:text-yellow-800 duration-200 underline">
+      <Link
+        href={"/"}
+        className="text-3xl font-bold hover:text-yellow-800 duration-200 underline"
+      >
         Sanity Blog
       </Link>
 
       <nav>
         <ul className="flex gap-6">
-          <li className="hover:underline font-bold  hover:text-cyan-600  duration-200 text-xl cursor-pointer">About</li>
-          <li className="hover:underline  font-bold hover:text-cyan-600 duration-200 text-xl cursor-pointer">Contact</li>
+          <Link href={"/about"}>
+            <li className="hover:underline font-bold  hover:text-cyan-600  duration-200 text-xl cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link href={"/contact"}>
+          <li className="hover:underline  font-bold hover:text-cyan-600 duration-200 text-xl cursor-pointer">
+            Contact
+          </li></Link>
         </ul>
       </nav>
     </header>
